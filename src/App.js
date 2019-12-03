25a8f1cd075757e53b36d1b6f565885873ebd09b
Marcel Balint <blank_blankness@yahoo.com>
2019-12-03 06:57:15 -0500

    Added google fonts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>Testing</h2>
+      <span className="fas fa-home"></span>
+    </div>
+  );
 }
 
 export default App;
