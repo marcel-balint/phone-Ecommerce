e81cb1b13af551a8ce8b25c700ad197470e86c61
Marcel Balint <blank_blankness@yahoo.com>
2020-01-16 09:48:10 -0500

    Added the cart footer which contains the product title and the price

--- src/components/Product.js
+++ src/components/Product.js
@@ -33,6 +33,13 @@ class Product extends Component {
               )}
             </button>
           </div>
+          <div className="card-footer d-flex justify-content-between">
+            <p className="align-self-center mb-0">{this.props.product.title}</p>
+            <h5 className="text-blue font-italic mb-0">
+              <span className="mr-1">$</span>
+              {this.props.product.price}
+            </h5>
+          </div>
         </div>
       </ProductWrapper>
     );
