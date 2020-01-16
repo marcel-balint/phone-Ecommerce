70da0ed084464e1358a6a1c815f994ba66437769
Marcel Balint <blank_blankness@yahoo.com>
2020-01-16 09:16:58 -0500

    Rendered the images on the products page

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
 
 class Product extends Component {
+  //const {id , title, img, price, inCart} = this.props.product;
   render() {
-    return <h3>The product </h3>;
+    return (
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+        <div className="card">
+          <div className="img-container p-5" onClick={console.log("clicked")}>
+            <Link to="/details">
+              <img
+                src={this.props.product.img}
+                alt="product"
+                className="card-img-top"
+              ></img>
+              <button className="cart-btn"></button>
+            </Link>
+          </div>
+        </div>
+      </ProductWrapper>
+    );
   }
 }
-
+const ProductWrapper = styled.div``;
 export default Product;
