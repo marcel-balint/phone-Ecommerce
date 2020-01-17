521b0e503e7f7b68d935b5308c29bd9ad9aca800
Marcel Balint <blank_blankness@yahoo.com>
2020-01-17 09:07:26 -0500

    Added the decrement button

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -14,6 +14,26 @@ const CartItem = ({ item, value }) => {
           alt="product"
         ></img>
       </div>
+      <div className="col-10 mx-auto col-lg-2">
+        <span className="d-lg-none">Product</span> {title}
+      </div>
+      <div className="col-10 mx-auto col-lg-2">
+        <span className="d-lg-none">Price</span> {price}
+      </div>
+      <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+        <div className="d-flex justify-content-center">
+          <div>
+            <span
+              className="btn btn-black mx-1"
+              onClick={() => {
+                decrement(id);
+              }}
+            >
+              -
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
