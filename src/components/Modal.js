370929051a3cf901988d646094072f0c12f45893
Marcel Balint <blank_blankness@yahoo.com>
2020-01-17 07:38:33 -0500

    Filled the modal with the data from a product

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -9,17 +9,61 @@ class Modal extends Component {
     return (
       <ProductConsumer>
         {value => {
-          const { modalOpen, closedModal } = value;
+          const { modalOpen, closeModal } = value;
           const { img, title, price } = value.modalProduct;
           if (!modalOpen) {
             return null;
           } else {
-            <ModalContainer></ModalContainer>;
+            return (
+              <ModalContainer>
+                <div className="container">
+                  <div className="row">
+                    <div
+                      id="modal"
+                      className="col-8 mx-auto col-md-6 col-lg-4 text-center py-5"
+                    >
+                      <h3>Item added</h3>
+                      <img src={img} className="img-fluid" alt="product"></img>
+                      <h5>{title}</h5>
+                      <h5 className="text-muted">Pice: ${price}</h5>
+                      <Link to="/">
+                        <ButtonContainer
+                          style={{ marginRight: 10 }}
+                          cart
+                          onClick={() => closeModal()}
+                        >
+                          Store
+                        </ButtonContainer>
+                      </Link>
+                      <Link to="/cart">
+                        <ButtonContainer cart onClick={() => closeModal()}>
+                          Go to cart
+                        </ButtonContainer>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </ModalContainer>
+            );
           }
         }}
       </ProductConsumer>
     );
   }
 }
+const ModalContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.3);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  #modal {
+    background: #fff;
+  }
+`;
 
 export default Modal;
