e8386f629101585cc9051bb6bc325aa241168120
Marcel Balint <blank_blankness@yahoo.com>
2020-03-02 04:43:30 -0500

    Added the paypal button

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -18,7 +18,7 @@ class Cart extends Component {
                   <Title title="The cart" />;
                   <CartColumns />
                   <CartList value={value} />
-                  <CartTotals value={value} />
+                  <CartTotals value={value} history={this.props.history} />
                 </React.Fragment>
               );
             } else {
