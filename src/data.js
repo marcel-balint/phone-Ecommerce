45a26ff8865d40c608f28c802d2d8f0093e36142
Marcel Balint <blank_blankness@yahoo.com>
2020-03-02 06:34:26 -0500

    Added more phones

--- src/data.js
+++ src/data.js
@@ -1,96 +1,144 @@
 export const storeProducts = [
   {
     id: 1,
-    title: "Google Pixel - Black",
-    img: "img/product-1.png",
-    price: 10,
-    company: "GOOGLE",
+    title: "Samsung Galaxy A70",
+    img: "img/3r.jpg",
+    price: 299,
+    company: "SAMSUNG",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Samsung Galaxy A70 (Dual SIM 6GB RAM 128GB 4G LTE) is a feature rich phablet that comes packed with some top notch specs. To start off, it comes with a huge 6.7-inch Super AMOLED capacitive touchscreen that produces 16M colours, 1080 x 2400 pixels resolution and 393ppi density. The display is perfect for playing HD games and watch videos. It is also effective for multitasking on the go. The phablet comes preloaded with Android 9.0 (Pie) and runs on Qualcomm SDM675 Snapdragon 675 (11 nm) chipset in conjunction with Octa-core (2x2.0 GHz Kryo 460 Gold & 6x1.7 GHz Kryo 460 Silver) processor and Adreno 612 GPU.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 2,
-    title: "Samsung S7",
-    img: "img/product-2.png",
-    price: 16,
-    company: "SAMSUNG",
+    title: "BlackBerry KEY2 LE",
+    img: "img/4r.jpg",
+    price: 769,
+    company: "BlackBerry",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "BlackBerry KEY2 LE (BBE100-4 4GB RAM 64GB 4G LTE) is a 2018-released QWERTY Android smartphone. Its a low-specs variant of the flagship BlackBerry KEY2, it comes with iconic design of U shape with physical keyboard and a soft textured back for a better grip. Featuring a 35-key Smart Keypad on the front which looks just like a miniature PC keyboard. The keypad allows quick and easy typing for work related operations such as emails and document edits. The handset boasts a compact 4.5-inches IPS LCD touchscreen that offers 1080 x 1620 pixels at 434 ppi density. It also features multitouch and comes with Corning Gorilla Glass protection.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 3,
-    title: "HTC 10 - Black",
-    img: "img/product-3.png",
-    price: 8,
-    company: "htc",
+    title: "Samsung Galaxy Note9",
+    img: "img/5r.jpg",
+    price: 989,
+    company: "SAMSUNG",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Samsung Galaxy Note9 (Dual SIM 128GB 4G LTE) is the next-generation premium flagship phablet in the Note series by Korean tech giant Samsung. This beast of a device boasts specs far better than all previous flagships and redefines consumer expectations. Boasting a aluminum frame and glass construction, the phone offers glass on the front and back, bend in unison creating a seamless and flush surface all around.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 4,
-    title: "HTC 10 - White",
-    img: "img/product-4.png",
-    price: 18,
-    company: "htc",
+    title: "Refurbished Apple iPhone 6",
+    img: "img/Ifo.jpg",
+    price: 878,
+    company: "APPLE",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "This is a Factory Refurbished model which comes with a 6 months warranty. It has gone through with a careful refurbishment process, full testing and a final factory inspection. It may have minor cosmetic blemishes but they will not affect the function of the device.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 5,
-    title: "HTC Desire 626s",
-    img: "img/product-5.png",
-    price: 24,
-    company: "htc",
+    title: "Samsung Galaxy S10 Plus",
+    img: "img/7er.jpg",
+    price: 949,
+    company: "SAMSUNG",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Samsung Galaxy S10 Plus (8GB RAM 128GB 4G LTE) is enhanced variant of next-generation flagship S10 from Korean tech giant Samsung. With its unique design this smartphone offers higher specs as compared to previous generations like improved hardware and Samsung's ultrasonic on-screen fingerprint sensor. It models an infinity-O display with less bezel at the top and front-facing camera embedded in the screen's top right corner. An 7.8mm thin and seamless body construction featuring a large 6.4-inch Super dynamic AMOLED display having a pixel density of 522ppi and a resolution of 1440 x 3040 pixels. The screen is made of Corning Gorilla Glass 6 and has an always-on display.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 6,
-    title: "Vintage Iphone",
-    img: "img/product-6.png",
-    price: 17,
-    company: "apple",
+    title: "iPhone 7 ",
+    img: "img/8er.jpg",
+    price: 759,
+    company: "APPLE",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Apple presents all new iPhone 7 from the companys famous flagship iPhone series. Released in September 2016, incorporating a premium build quality with some advance specifications. The handy design and shape allow comfortable usage while held in hand. The handset sports a 4.7-inch LED-backlit IPS LCD touchscreen with 1334 x 750 pixels resolution, 326ppi pixel density and Wide Gamut display. The display is protected with oleophobic coating and Ion-strengthened glass to protect the phone from bumps while the water and dust resistant structure makes it safe from splashes and dust elements.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 7,
-    title: "Iphone 7",
-    img: "img/product-7.png",
-    price: 30,
+    title: "Iphone X",
+    img: "img/9er.jpg",
+    price: 232,
     company: "apple",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "iPhone X (256GB 4G LTE) is the 2017 released next-generation iPhone from famous American device maker Apple. It is the biggest version among the three variants released this year, but features the same internal hardware specs as the others. The smartphone provides great improvement in specs as compared to its predecessor model. It now features a powerful 12MP wide-angle main camera with built-in Dual Optical Image Stabilization, optical/digital zoom up to 10x and Quad LED flash with Slow Sync mode. It even provides a special portrait mode and portrait lighting (beta). It is capable of recording 4K/Ultra HD 2160p movies at 24/30/60fps along with up to 6x digital zoom, Time Lapse and Slow Motion video support at 1080p. A 7MP TrueDepth front camera is also provided with Retina Flash and Full HD capability. Unlike other iPhone variants, the front camera of iPhone X also features portrait mode and portrait lighting (beta) along with Animoji support.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 8,
-    title: "Smashed Iphone",
-    img: "img/product-8.png",
-    price: 2,
+    title: "Sony Xperia Z5",
+    img: "img/10e.jpg",
+    price: 529,
     company: "apple",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Sony Xperia Z5 Dual E6633 4G LTE is the cutting-edge flagship smartphone announced in September and released in October 2015. The handset offers dust proof and water resistant functionalities for free hand use. Coming with an elegant round-edge design, the new smartphone features a large 5.2-inch IPS LCD capacitive touch display supporting 16M colours, 1920x1080 pixels resolution and a 424ppi pixel density. The multi-touch display employs scratch-resistant glass material with oleophoic coating, providing a solid protection.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 9,
+    title: "HTC U12+",
+    img: "img/11er.jpg",
+    price: 419,
+    company: "apple",
+    info:
+      "HTC U12+ (128GB 4G LTE) is the next-generation model of the large-screen HTC U11+ smartphone. Like its predecessor, this handset is also a multimedia powerhouse primarily built for great movie viewing and gaming. It features a 6.0-inch Super LCD6 display that supports WQHD+ resolution of 2280 x 1440 pixels. Playing games and watching videos is amazing with a screen so big.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 10,
+    title: "BlackBerry Evolve",
+    img: "img/2n.jpg",
+    price: 169,
+    company: "apple",
+    info:
+      "BlackBerry Evolve (4GB RAM 64GB 4G LTE) offers lightweight and slim profile, featuring series 7 aluminum frame for stylish as well as durable contour. It sports a large 5.99-inch IPS LCD capacitive touchscreen with 1080 x 2160 pixels resolution at 403 ppi pixel density and supports multi-touch. The display is protected with Corning Gorilla Glass 5 and features a specialized Oleophobic coating to protect screen against daily wear and tear.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 11,
+    title: "Sony Xperia 5",
+    img: "img/sony.jpg",
+    price: 499,
+    company: "sony",
+    info:
+      "Sony Xperia 5 (J9210 Dual SIM 6GB RAM 128GB 4G LTE) is a premium quality flagship phone from Sony. Engineered to perfection, the phone features optimal size to fit in your hand. Available in vibrant colours, the phone showcases a 6.1-inch FHD+ HDR OLED display that produces 1080 x 2520 pixels, 16M colours and 449ppi density. Everything on this display looks amazing under all lighting conditions. You can enjoy movies, games and much more anywhere you want.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 12,
+    title: "Huawei Mate 20 Pro",
+    img: "img/huaw.jpg",
+    price: 399,
+    company: "sony",
+    info:
+      "Huawei Mate 20 Pro 6GB RAM 128GB is the latest flagship smartphone based on limitless human imagination. The slim and sleek design of the aluminum frame makes it a treat to touch and watch. It has a spectacular contour that features an iconic square combining Leica Triple Camera. The natural masterpiece showcases a gigantic 6.39 inches AMOLED capacitive touchscreen with 16M colours, 1440 X 3120 pixels resolution and 538ppi pixel density. ",
     inCart: false,
     count: 0,
     total: 0
