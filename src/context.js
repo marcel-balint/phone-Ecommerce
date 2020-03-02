5d6143b4efd0a8b7a2d6b203f09876b81f967659
Marcel Balint <blank_blankness@yahoo.com>
2020-03-02 03:42:21 -0500

    Created the removeItem function which removes a particular item from cart

--- src/context.js
+++ src/context.js
@@ -80,7 +80,29 @@ class ProductProvider extends Component {
 
   decrement = id => {};
 
-  removeItem = id => {};
+  removeItem = id => {
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+
+    tempCart = tempCart.filter(item => item.id !== id);
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index];
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+
+    this.setState(
+      () => {
+        return {
+          cart: [...tempCart],
+          products: [...tempProducts]
+        };
+      },
+      () => {
+        this.addTotals();
+      }
+    );
+  };
 
   clearCart = () => {
     this.setState(
