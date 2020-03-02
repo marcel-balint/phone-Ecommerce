6856e3d3973f93e6a8ee9ab5c0aa348f64805f09
Marcel Balint <blank_blankness@yahoo.com>
2020-03-02 04:21:29 -0500

    Styled the default component which displays the 404 error

--- src/components/Default.js
+++ src/components/Default.js
@@ -3,8 +3,19 @@ import React, { Component } from "react";
 class Default extends Component {
   render() {
     return (
-      <div>
-        <h3>Page not found.</h3>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto text-center pt-4">
+            <h2 className="display-3">404</h2>
+            <h3>
+              The requested URL{" "}
+              <span className="text-danger">
+                "{this.props.location.pathname}"
+              </span>{" "}
+              was not found
+            </h3>
+          </div>
+        </div>
       </div>
     );
   }
