d588ea25324d4cc8e125137bce33b6f72c9db5b5
Marcel Balint <blank_blankness@yahoo.com>
2020-03-02 03:05:52 -0500

    Created the addTotals function which adds the tax and calculates the total cart

--- src/components/cart/CartTotals.js
+++ src/components/cart/CartTotals.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function CartTotals({ value }) {
-  const { cartSubtotal, cartTax, cartTotal, clearCart } = value;
+  const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
   return (
     <React.Fragment>
       <div className="container">
@@ -19,7 +19,7 @@ export default function CartTotals({ value }) {
             </Link>
             <h5>
               <span>subtotal: </span>
-              <strong>$ {cartSubtotal}</strong>
+              <strong>$ {cartSubTotal}</strong>
             </h5>
             <h5>
               <span>tax: </span>
